cfb8cf23eb45ebb643d22bec028c6e9695b71458
PizzerSpring <vladi-slav96@mail.ru>
2022-07-24 15:17:02 +0700

    editing FOOTER

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,46 +1,47 @@
 import React from 'react';
 import style from './Footer.module.css';
+import containerStyle from './../../commons/Container.module.css';
 
 const Footer = () => {
     return (
         <div className={style.footer}>
-            <h4 className={style.footerH4}>GET IN TOUCH</h4>
-            <div className={style.footercontainer}>
+            <h2 className={style.title}>GET IN TOUCH</h2>
+            <div className={containerStyle.container}>
                 <div>
-                    <h5>DON'T BE SHY !</h5>
-                    <p className={style.footertext}>
+                    <h3 className={style.textTitle}>DON'T BE SHY !</h3>
+                    <p className={style.text}>
                         Feel free to get in touch with me. I am always<br/> open to discussing new projects, creative ideas or<br/> opportunities to be part of your visions.
                     </p>
-                    <div className={style.footeryellowiconscontainer}>
-                        <div className={style.footeryellowicons}></div>
+                    <div className={style.yellowIconsContainer}>
+                        <div className={style.yellowIcons}></div>
                         <div>
                             <p>MAIL ME</p>
                             <span>vladislav9646@gmail.com</span>
                         </div>
                     </div>
-                    <div className={style.footeryellowiconscontainer}>
-                        <div className={style.footeryellowicons}></div>
+                    <div className={style.yellowIconsContainer}>
+                        <div className={style.yellowIcons}></div>
                         <div>
                             <p>CALL ME</p>
                             <span>+7 961 860 05 12</span>
                         </div>
                     </div>
-                    <div className={style.footersocialiconscontainer}>
-                        <div className={style.footersocialicons}></div>
-                        <div className={style.footersocialicons}></div>
-                        <div className={style.footersocialicons}></div>
-                        <div className={style.footersocialicons}></div>
+                    <div className={style.socialIconsContainer}>
+                        <div className={style.socialIcons}></div>
+                        <div className={style.socialIcons}></div>
+                        <div className={style.socialIcons}></div>
+                        <div className={style.socialIcons}></div>
                     </div>
                 </div>
-                <div className={style.footerform}>
+                <div className={style.form}>
                     <form action="">
-                        <div className={style.footerinputscontainer}>
-                            <input className={style.footerinputs} placeholder='YOUR NAME' type="text"/>
+                        <div className={style.inputsContainer}>
+                            <input className={style.input} placeholder='YOUR NAME' type="text"/>
                             <input placeholder='YOUR EMAIL' type="email"/>
                             <input placeholder='YOUR SUBJECT' type="text"/>
                         </div>
                         <div>
-                            <textarea placeholder='YOUR MESSAGE' name="" id="" className={style.footerinputtextarea}></textarea>
+                            <textarea placeholder='YOUR MESSAGE' name="" id="" className={style.textarea}></textarea>
                         </div>
                         <div>
                             <a href="">SEND MESSAGE</a>
