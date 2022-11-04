5db684e91f0d13728ed1b05bc4a5d5936b93404e
PizzerSpring <vladi-slav96@mail.ru>
2022-11-04 13:54:02 +0700

    fix radius My Skills

--- src/components/Myskills/Myskills.tsx
+++ src/components/Myskills/Myskills.tsx
@@ -9,20 +9,20 @@ const Myskills = () => {
             <h3 className={style.title}>MY SKILLS</h3>
             <div className={containerStyle.container}>
                 <div>
-                    <SkillsCard count={'80%'} title={'HTML'}/>
-                    <SkillsCard count={'60%'} title={'CSS'}/>
+                    <SkillsCard count={'80'} title={'HTML'}/>
+                    <SkillsCard count={'60'} title={'CSS'}/>
                 </div>
                 <div>
-                    <SkillsCard count={'50%'} title={'JAVASCRIPT'}/>
-                    <SkillsCard count={'70%'} title={'TYPESCRIPT'}/>
+                    <SkillsCard count={'60'} title={'JAVASCRIPT'}/>
+                    <SkillsCard count={'70'} title={'TYPESCRIPT'}/>
                 </div>
                 <div>
-                    <SkillsCard count={'40%'} title={'REACT'}/>
-                    <SkillsCard count={'20%'} title={'JQUERY'}/>
+                    <SkillsCard count={'70'} title={'REACT'}/>
+                    <SkillsCard count={'20'} title={'JQUERY'}/>
                 </div>
                 <div>
-                    <SkillsCard count={'90%'} title={'REDUX'}/>
-                    <SkillsCard count={'5%'} title={'PHP'}/>
+                    <SkillsCard count={'90'} title={'REDUX'}/>
+                    <SkillsCard count={'5'} title={'PHP'}/>
                 </div>
             </div>
         </div>
--- src/components/Myskills/SkillsCard.tsx
+++ src/components/Myskills/SkillsCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Myskills.module.scss';
 
 type SkillsCardPropsType = {
@@ -7,14 +7,21 @@ type SkillsCardPropsType = {
 }
 
 const SkillsCard = (props: SkillsCardPropsType) => {
+    const pct = ((100 - +props.count) / 100) * Math.PI * (90 * 2);
+    const styleStrokeDashoffset = {
+        strokeDashoffset: `${pct}`
+    }
+
+
     return (
         <div className={style.skillCard}>
-            <div className={style.skills}>
-                <span className={style.skillsLevel}>{props.count}</span>
-                <div className={style.slice}>
-                    <div className={style.bar}></div>
-                    <div className={style.fill}></div>
-                </div>
+            <div className={style.cont} data-pct={props.count}>
+                <svg className={style.svg} width="200" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <circle style={styleStrokeDashoffset} r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48"
+                            stroke-dashoffset="0"></circle>
+                    <circle style={styleStrokeDashoffset} className={style.bar} r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48"
+                            stroke-dashoffset="0"></circle>
+                </svg>
             </div>
             <h6 className={style.skillTitle}>{props.title}</h6>
         </div>
