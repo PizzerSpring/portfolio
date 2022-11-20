0e478a5704c05262e217a85611c2be27ef5003a8
PizzerSpring <vladi-slav96@mail.ru>
2022-11-20 13:57:46 +0700

    adding links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Footer.module.scss';
 import containerStyle from './../../commons/Container.module.css';
 import telegramIcon from './../../assets/images/telegram.png';
-import facebookIcon from "../../assets/images/facebook.png";
+import vkIcon from "../../assets/images/vk.png";
 import twitterIcon from "../../assets/images/twitter.png";
 import youtubeIcon from "../../assets/images/youtube.png";
-import pinterestIcon from "../../assets/images/pinterest.png";
+import hhIcon from "../../assets/images/hhru.png";
 import mailmeIcon from "../../assets/images/mailme.png";
 import callmeIcon from "../../assets/images/callme.png";
 
 const Footer = () => {
+    const [isMessageActive, setIsMessageActive] = useState<boolean>(false);
     const telegram = {
         backgroundImage: `url(${telegramIcon})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
     }
-    const facebook = {
-        backgroundImage: `url(${facebookIcon})`,
+    const vk = {
+        backgroundImage: `url(${vkIcon})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
     }
@@ -30,8 +31,8 @@ const Footer = () => {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
     }
-    const pinterest = {
-        backgroundImage: `url(${pinterestIcon})`,
+    const hh = {
+        backgroundImage: `url(${hhIcon})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
     }
@@ -76,23 +77,23 @@ const Footer = () => {
                     </p>
                     <div className={style.socialIconsContainer}>
                         <ul>
-                            <li style={facebook}>
-                                <a href="">
+                            <li style={vk}>
+                                <a href="https://vk.com/vladislav_guschin">
                                     <span></span>
                                 </a>
                             </li>
                             <li style={twitter}>
-                                <a href="">
+                                <a href="#">
                                     <span></span>
                                 </a>
                             </li>
                             <li style={youtube}>
-                                <a href="">
+                                <a href="https://www.youtube.com/">
                                     <span></span>
                                 </a>
                             </li>
-                            <li style={pinterest}>
-                                <a href="">
+                            <li style={hh}>
+                                <a href="https://novokuznetsk.hh.ru/resume/40f12a37ff08aedfb70039ed1f7852384d7733">
                                     <span></span>
                                 </a>
                             </li>
@@ -100,7 +101,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className={style.form}>
-                    <form action="">
+                    <form action="" onSubmit={(e) => {e.preventDefault()}}>
                         <div className={style.inputsContainer}>
                             <div className={style.elementInput}>
                                 <input className={style.input} placeholder='YOUR NAME' type="text"/>
@@ -114,13 +115,15 @@ const Footer = () => {
                         </div>
                         <div>
                             <textarea placeholder='YOUR MESSAGE' name="" id="" className={style.textarea}></textarea>
-                            <button className={style.button}>
+                            <button className={style.button} onClick={() => {
+                                console.log(isMessageActive)
+                                setIsMessageActive(true)}}>
                                 <span className={style.buttonText}>send message</span>
                                 <span className={style.buttonIcon} style={telegram}></span>
                             </button>
                         </div>
                         <div>
-                            <span></span>
+                            {isMessageActive ? <span className={style.outputMessage}>Message sent!</span> : ''}
                         </div>
                     </form>
                 </div>
